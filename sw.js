bce379701183a4498d2bc53cc2a796b50058b379
disobey607-sketch <disobey607-sketch@github.com>
2026-03-08 20:00:52 +0900

    Initial commit for AI Blog Maker

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'ai-blog-v1';
+const urlsToCache = [
+  './블로그 자동화.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 캐시에 있으면 반환, 없으면 네트워크 요청
+        return response || fetch(event.request);
+      })
+  );
+});
